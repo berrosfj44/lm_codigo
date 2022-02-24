ed74c5f65495b56c716a527523c8cea5b13c6c17
berrosfj <a_fraberros@iestriana.com>
2022-02-24 12:51:47 +0100

    d

---
+++ practicas/js/ejercicio04/js/funciones.js
@@ -0,0 +1,79 @@
+/* function numero() {
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+    var numero2 = document.getElementById("numero3").value;
+
+if (numero1 > numero2 and numero1 > numero3) {
+    var mayor = numero1;
+}
+
+else if (numero1 < numero2 and numero1 < numero3) {
+    var menor = numero1;
+
+} 
+
+else if (numero1 < numero2 and numero1 > numero3) {
+    var centro = numero1;
+
+} 
+
+else if (numero1 < numero2 and numero1 < numero3) {
+    var centro = numero1;
+
+} 
+
+else if (numero1 > numero2 and numero1 < numero3) {
+    var centro = numero1;
+
+} 
+
+
+if (numero2 > numero1) {
+    var mayor = numero2;
+} 
+else{
+    var menor = numero2;
+
+} 
+else if (numero2 > numero3) {
+    var mayor = numero2;
+
+} 
+else {
+    var centro = numero2;
+}
+
+
+if (numero3 > numero1) {
+    var mayor = numero3;
+} 
+else{
+    var menor = numero3;
+
+} 
+else if (numero3 > numero2) {
+    var mayor = numero3;
+
+} 
+else {
+    var centro = numero3;
+}
+
+document.getElementById("mayor").innerHTML = mayor; 
+document.getElementById("menor").innerHTML = menor; 
+document.getElementById("central").innerHTML = central; 
+
+
+
+ */
+
+
+/* else if (numero1 < numero2) {
+    document.getElementById("resultado").innerHTML = "el segundo numero es mayor"; 
+  } else if (){
+    document.getElementById("resultado").innerHTML = "el tercer numero es mayor"; 
+  }
+
+
+
+} */
