a23482710ffe10470ce96037bf6b7f2fac998c71
berrosfj <a_fraberros@iestriana.com>
2022-03-24 13:42:17 +0100

    exam

--- examen/examen-js/ejercicio02/js/funciones.js
+++ examen/examen-js/ejercicio02/js/funciones.js
@@ -2,5 +2,4 @@ function mostrar() {
     document.getElementById("uno").className = "uno";
     document.getElementById("dos").className = "dos";
     document.getElementById("tres").className = "tres";
-
 }
